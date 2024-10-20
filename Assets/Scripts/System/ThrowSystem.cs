48e9b240367b41cbb48121156c720e3ec7b3f811
Hienducnguyen1206 <hiennd.nguyenduc2001@gmail.com>
2024-10-20 16:06:24 +0700

    Update ThrowSystem.cs

--- Assets/Scripts/System/ThrowSystem.cs
+++ Assets/Scripts/System/ThrowSystem.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThrowSystem : MonoBehaviour
 {
+    [SerializeField]
     [Range(-30f,30f)] float DiscThrowDeg = 0f ;
+    [SerializeField]
+    [Range(0, 10f)] float Offset = 0f;
+    [SerializeField]
     [Range(0, 10f)] float ThrowPower = 0f;
-    
+
+    [SerializeField] Slider OffSet;
+    [SerializeField] Slider Angle;
+    [SerializeField] Slider Power;
+
     void Start()
     {
         
@@ -16,6 +25,11 @@ public class ThrowSystem : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void ChangeDiscState()
+    {
+
     }
 
     public void ChangeDisc() { }
