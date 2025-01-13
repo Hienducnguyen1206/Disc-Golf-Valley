823e024937dd5d4e649ff750e8d97158edc0b61a
MinhPhambk <phamngocminh1230@gmail.com>
2025-01-13 20:45:12 +0700

    Add Canvas

---
+++ Assets/Ilumisoft/Dusty Canyon/Scripts/UI/CanvasWelcome.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasWelcome : UICanvas
+{
+    public void StartButton()
+    {
+        GameManager.Ins.ChangeState(GameState.Playing);
+        GameManager.Ins.SetActiveGamePlay(true);
+        // GUIManager.Ins.OpenUI<CanvasInfo>();
+        Close(0);
+    }
+
+    public void ExitButton()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+
+        Close(0);
+    }
+}
+
