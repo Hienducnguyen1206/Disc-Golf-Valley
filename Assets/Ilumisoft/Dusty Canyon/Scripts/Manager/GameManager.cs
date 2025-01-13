b7f26cfd7be35cc43fd32719df494b8bcbd7f828
MinhPhambk <phamngocminh1230@gmail.com>
2025-01-13 10:03:26 +0700

    Add GamePlay

---
+++ Assets/Ilumisoft/Dusty Canyon/Scripts/Manager/GameManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public GameObject MapPrefab;
+
+    public int Score;
+
+    private GameObject mapObject;
+
+    private GameState gameState;
+
+    private Transform zeusTransform;
+
+    private void Start()
+    {
+        if (mapObject != null)
+        {
+            Destroy(mapObject);
+        }
+
+        mapObject = Instantiate(MapPrefab, Vector3.zero, Quaternion.identity);
+        zeusTransform = mapObject.transform.Find("StartPoint/zeus");
+        Transform gameplayTransform = mapObject.transform.Find("GUI");
+        gameplayTransform.gameObject.SetActive(false);
+        Invoke("ShowWelcomeUI", 1.5f);
+    }
+
+    private void ShowWelcomeUI()
+    {
+        ChangeState(GameState.Start);
+        GUIManager.Ins.OpenUI<CanvasWelcome>();
+    }
+
+    private void Update()
+    {
+        if (GameManager.Ins.IsState(GameState.Playing) && zeusTransform != null)
+        {
+            Rigidbody rb = zeusTransform.GetComponent<Rigidbody>();
+
+            if (rb.velocity.magnitude > 0.01f && rb.velocity.magnitude < 0.1f) 
+            {
+                Vector3 basketPoint = mapObject.transform.Find("basket").position;
+                Vector3 startPoint = new Vector3(-85.099f, 36f, -282.8994f);
+                Vector3 currentPoint = zeusTransform.position;
+                float totalDistance = Vector3.Distance(startPoint, basketPoint);
+                float currentDistance = Vector3.Distance(startPoint, currentPoint);
+                
+                if (currentDistance > totalDistance)
+                {
+                    Score = 0;
+                }
+                else {
+                    Score = Mathf.RoundToInt(Mathf.Clamp01((totalDistance - currentDistance) / totalDistance) * 10);
+                }
+            }
+
+            if (rb.velocity.magnitude > 0 && rb.velocity.magnitude < 0.01f)
+            {
+
+                GUIManager.Ins.OpenUI<CanvasScore>();
+            }
+        }
+    }
+
+    public void Init()
+    {
+        if (mapObject != null)
+        {
+            Destroy(mapObject);
+        }
+        mapObject = Instantiate(MapPrefab, Vector3.zero, Quaternion.identity);
+        zeusTransform = mapObject.transform.Find("StartPoint/zeus");
+    }
+
+    public void SetActiveGamePlay(bool active)
+    {
+        mapObject.transform.Find("GUI").gameObject.SetActive(active);
+    }
+
+    public void ChangeState(GameState gameState)
+    {
+        this.gameState = gameState;
+    }
+
+    public bool IsState(GameState gameState)
+    {
+        return this.gameState == gameState;
+    }
+
+    public string GetScore()
+    {
+        return Score.ToString();
+    }
+}
---
+++ Assets/Ilumisoft/Dusty Canyon/Scripts/UI/CanvasScore.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CanvasScore : UICanvas
+{
+    public TMP_Text scoreText;
+
+    public void ResumeButton()
+    {
+        GameManager.Ins.ChangeState(GameState.Playing);
+        GameManager.Ins.Init();
+        Close(0);
+    }
+
+    public void ExitButton()
+    {
+        GameManager.Ins.ChangeState(GameState.Start);
+        GUIManager.Ins.OpenUI<CanvasWelcome>();
+        Close(0);
+    }
+
+    private void Awake()
+    {
+        Debug.Log("Score: " + GameManager.Ins.GetScore());
+        scoreText.text = GameManager.Ins.GetScore();
+    }
+
+    private void Start()
+    {
+        Debug.Log("Score: " + GameManager.Ins.GetScore());
+        scoreText.text = GameManager.Ins.GetScore();
+    }
+}
